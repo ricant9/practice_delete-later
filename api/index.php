dc486a43c4b5ad212bea4ba977a9fc214d7aa588
ricant9 <ericagumapac38@gmail.com>
2026-05-20 14:18:17 +0800

    feat: fix another problem

--- api/index.php
+++ api/index.php
@@ -1,9 +1,20 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+    $dotenvPath = dirname(__DIR__).'/.env';
+    if (file_exists($dotenvPath)) {
+        (new Dotenv())->loadEnv($dotenvPath);
+    }
+}
+
 return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $env = $context['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+    $debug = filter_var($context['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN);
+
+    return new Kernel($env, $debug);
 };
